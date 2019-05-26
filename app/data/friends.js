2359bd5ef126d6c3f8fedfc1326f7b9fbd11fabf
Natasha Giusti <natashagiusti@tashs-mac.lan>
2019-05-25 18:27:59 -0700

    data added to friends

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+// friends data
+var friends = [
+    {
+        "name": "Linda Belcher",
+        "photo": "https://kenpire.files.wordpress.com/2014/11/linda-alright-sweater.jpg",
+        "scores": []
+    },
+    {
+        "name": "Ben Wyatt",
+        "photo": "https://vignette.wikia.nocookie.net/parksandrecreation/images/0/0a/Ben.jpg/revision/latest?cb=20110809181418",
+        "scores": []
+    },
+    {
+        "name": "Sansa Stark",
+        "photo": "https://cdn.vox-cdn.com/thumbor/rY89FcENN9_OdXkNBpTDlovAmAo=/0x0:3155x2100/1200x800/filters:focal(1312x535:1816x1039)/cdn.vox-cdn.com/uploads/chorus_image/image/63869037/_20__Helen_Sloan___HBO.0.jpg",
+        "scores": []
+    },
+    {
+        "name": "Jake Peralta",
+        "photo": "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-Brooklyn-99-Andy-Sanberg.jpg",
+        "scores": []
+    },
+    {
+        "name": "Todd Chavez",
+        "photo": "https://2.bp.blogspot.com/-11gvjUTd4Ao/WTn9UG1JXpI/AAAAAAAAHGE/ePwZvWz989MVhK5BCo80Sj16MU6DAGvBgCLcB/s1600/BoJack%2BHorseman%2B-%2BTodd.jpg",
+        "scores": []
+    },
+    {
+        "name": "Hermione Granger",
+        "photo": "https://medias.spotern.com/spots/w640/45594-1532336916.jpg",
+        "scores": []
+    }
+];
+
+module.exports = friends;
